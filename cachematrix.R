7e04cb7c5452314fe23568eaa4c868d414eb5fa9
Marcelo Shiba <marcelo.shiba@gmail.com>
2015-06-18 10:31:57 -0300

    Include comments in functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function:
 # This function creates a special "matrix" object that can cache its inverse.
-# Returns a list containing the matrix and its inverse in cache
+# Return a list containing functions to access matrix or its inverse (from
+# cache, once its calculated)
 makeCacheMatrix <- function(x = matrix()) {
     # initialize cache as NULL
     cacheInverse <- NULL
@@ -18,10 +18,10 @@ makeCacheMatrix <- function(x = matrix()) {
     # get matrix
     get <- function() x
 
-    # set inverse matrix
+    # set inverse matrix in cache
     setInverseMatrix <- function(inverseMatrix) cacheInverse <<- inverseMatrix
 
-    # get inverse matrix
+    # get inverse matrix from cache
     getInverseMatrix <- function() { cacheInverse }
 
     # return list for functions associated to matrix or inverseMatrix
@@ -32,18 +32,25 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+# Get inverse matrix.
+# If it's already on cache, gets from cache. Otherwise, calculate it and store
+# it in cache.
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
+    # Try to get inverse from cache
     inverseMatrix <- x$getInverseMatrix()
-    # if it's null, then I need to calculate its inverse
+    # if it's null, then it's the first time since
+    # the matrix has been set. So I need to calculate its inverse
     if(is.null(inverseMatrix)) {
-        message("calculating cached data")
+        message("calculating inverse matrix and caching it")
         # get matrix
         data <- x$get()
-        # calculate its inverse
+        # calculate its inverse using sol
         inverseMatrix <- solve(data)
+        # save it in cache
         x$setInverseMatrix(inverseMatrix)
     }
+    # Return inverse matrix, that was already on cache or has been calculated
+    # right now and saved in cache to be used in next call.
     inverseMatrix
 }
